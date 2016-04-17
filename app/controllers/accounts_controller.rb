15297bd2932f2ae5e567b2a2cbc9b25c344192a6
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2016-04-17 22:29:12 +1200

    changed the destroy redirect for better flow

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -56,7 +56,7 @@ class AccountsController < ApplicationController
   def destroy
     @account.destroy
     respond_to do |format|
-      format.html { redirect_to accounts_url, notice: 'Account was successfully destroyed.' }
+      format.html { redirect_to root_url, notice: 'Account was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
